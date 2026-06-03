f17e938d74b3f6aabf2d9a44f2f0e3cea2a45661
Komo04 <kusumawiranata7@gmail.com>
2026-06-03 08:44:54 +0800

    update about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ function About() {
             <div className=" max-w-4xl mx-auto px-6 text-center">
                 <h1 className="text-3xl md:text-4xl lg:text-5xl  font-bold mb-6">About Me</h1>
                 <p className="text-base md:text-lg  mb-6">
-                    Web Developer & AI Project
+                    Web Developer
                     Project Pribadi & Akademik | 2024 – 2026
                     Mengembangkan website berbasis web menggunakan HTML, CSS, JavaScript, dan framework modern.
                     Membuat sistem informasi akademik berbasis website sebagai project tugas akhir.
